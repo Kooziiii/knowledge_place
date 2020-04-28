8787db706afd96d829469ca4d25592b63492f400
小田　幸治 <swimmer0206@icloud.com>
2020-04-28 22:42:58 +0900

    ヘッダー編集

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Navbar></Navbar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -7,10 +8,13 @@
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
-
+  components:{
+    Navbar
+  },
   data: () => ({
     //
   }),
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase">
+                <span>知識の溜まり場</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey"><span>Sign Out</span></v-btn>
+            <v-btn><span>Sign In</span></v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
