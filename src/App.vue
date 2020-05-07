1d2becd9bfe66336fdfd6f91c211c0f05fb32b02
小田　幸治 <swimmer0206@icloud.com>
2020-05-07 23:19:45 +0900

    メインページ作成

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Navbar></Navbar>
     <v-content>
       <router-view></router-view>
+      <Sidebar></Sidebar>
     </v-content>
 
     <v-footer class="px-0 py-0" color="black">
@@ -14,10 +15,12 @@
 <script>
   import Navbar from '@/components/Navbar'
   import Footer from "./components/Footer";
+  import Sidebar from "./components/Sidebar";
 
 export default {
   name: 'App',
   components:{
+    Sidebar,
     Footer,
     Navbar
   },
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="8">
+                <v-card>
+                    <v-tabs
+                            background-color="white"
+                            color="deep-purple accent-4"
+                            right
+                    >
+                        <v-tab>Landscape</v-tab>
+                        <v-tab>City</v-tab>
+                        <v-tab>Abstract</v-tab>
+                        <v-tab>None</v-tab>
+                        <v-tab-item
+                                v-for="n in 4"
+                                :key="n"
+                        >
+                            <v-container fluid>
+                                <v-row>
+                                    <v-col
+                                            height="100px"
+                                            v-for="i in 6"
+                                            :key="i"
+                                            cols="12"
+                                            md="6"
+                                    >
+                                        <v-img
+                                                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                                                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                                                aspect-ratio="1"
+                                        ></v-img>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-tab-item>
+                    </v-tabs>
+                </v-card>
+            </v-col>
+            <v-col cols="4">
+                <p>dododo</p>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Sidebar"
+    }
+</script>
+
+<style scoped>
+
+</style>
