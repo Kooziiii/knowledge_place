e481ef32fda39c1fdca7be6a03e2ca86bba34736
小田　幸治 <swimmer0206@icloud.com>
2020-05-09 22:56:14 +0900

    微調整

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,19 +3,12 @@
         <v-row>
             <v-col cols="8">
                 <v-card>
-                    <v-tabs
-                            background-color="white"
-                            color="deep-purple accent-4"
-                            right
-                    >
+                    <v-tabs background-color="white" color="deep-purple accent-4" right>
                         <v-tab>Landscape</v-tab>
                         <v-tab>City</v-tab>
                         <v-tab>Abstract</v-tab>
                         <v-tab>None</v-tab>
-                        <v-tab-item
-                                v-for="n in 4"
-                                :key="n"
-                        >
+                        <v-tab-item v-for="n in 4" :key="n">
                             <v-container fluid>
                                 <v-row>
                                     <v-col
