8a004656968189adc5286a433fb916b4e85a3701
小田　幸治 <swimmer0206@icloud.com>
2020-05-10 23:34:46 +0900

    微調整

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -11,13 +11,7 @@
                         <v-tab-item v-for="n in 4" :key="n">
                             <v-container fluid>
                                 <v-row>
-                                    <v-col
-                                            height="100px"
-                                            v-for="i in 6"
-                                            :key="i"
-                                            cols="12"
-                                            md="6"
-                                    >
+                                    <v-col height="100px" v-for="i in 6" :key="i" cols="12" md="6">
                                         <v-img
                                                 :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
                                                 :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
@@ -31,7 +25,7 @@
                 </v-card>
             </v-col>
             <v-col cols="4">
-                <p>dododo</p>
+                <p>メイン記事</p>
             </v-col>
         </v-row>
     </v-container>
