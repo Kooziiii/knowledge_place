f98fad5a3800008963a94f8d2a83d21592103a7e
小田　幸治 <swimmer0206@icloud.com>
2020-05-12 23:56:14 +0900

    pagenationの作成

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,4 +1,25 @@
 <template>
+    <!-- ページネーションの設定-->
+    <v-content>
+        <div class="text-center">
+            <v-list>
+                <v-list-item v-for="list in displayLists" :key="list.index">
+                    <v-list-item-content>
+                        <v-list-item-title>{{ list.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            <v-pagination
+                    class="pagination"
+                    :total-visible="7"
+                    v-model="page"
+                    :length="length"
+                    @input = "pageChange"
+            ></v-pagination>
+        </div>
+    </v-content>
+    <!--
     <v-container>
         <v-row>
             <v-col cols="8">
@@ -28,15 +49,39 @@
                 <p>メイン記事</p>
             </v-col>
         </v-row>
-    </v-container>
+    </v-container>-->
 </template>
 
 <script>
     export default {
-        name: "Sidebar"
+        name: "Sidebar",
+        data () {
+            return {
+                page: 1,
+                lists: [],
+                displayLists: [],
+                pageSize: 10,
+            }
+        },
+        methods: {
+            pageChange: function(pageNumber){
+                this.displayLists = this.lists.slice(this.pageSize*(pageNumber -1), this.pageSize*(pageNumber));
+            }
+        },
+        mounted: function(){
+            this.lists = new Array(99).fill().map((v,i)=> {
+                return { id : i,title : 'Title' + i };
+            });
+
+            this.length = Math.ceil(this.lists.length/this.pageSize);
+
+            this.displayLists = this.lists.slice(this.pageSize*(this.page -1), this.pageSize*(this.page));
+        }
     }
 </script>
 
 <style scoped>
-
+    .pagination :focus{
+        outline: none;
+    }
 </style>
